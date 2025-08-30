16d2e4fda252676e87e21b18f4cf9baa140543e7
ByteDrew <199672192+ByteDrew@users.noreply.github.com>
2025-08-29 21:23:46 -0500

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 const footer = `
 <footer>
     <p>
-        2025 â€” forevermore <i>/</i> lovingly coded by <a href="https://justinjackson.ca/webmaster/">aid</a> / <a href="https://zhongvie.neocities.org/credits">credits</a> / <a href="https://asuraid.atabook.org/">guestbook</a> / <a href="mailto:sylviegirly@pm.me">contact me</a>
+        2025 forevermore <i>/</i> lovingly coded by <a href="https://justinjackson.ca/webmaster/">drew</a> / <a href=".../credits.html">credits</a> / <a href="https://bytedrew.atabook.org/">guestbook</a> / <a href="mailto:bytedrew@proton.me">contact me</a>
     </p>
 </footer>
 `;
