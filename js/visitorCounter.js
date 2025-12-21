df32fe9269b96d29aa136d0d444c3da82610bf98
ByteDrew <bytedrew@outlook.com>
2025-12-20 23:52:13 -0600

    Update visitor count API endpoint and data handling

--- js/visitorCounter.js
+++ js/visitorCounter.js
@@ -2,8 +2,8 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var site_data = JSON.parse(this.responseText);
-    document.getElementById("visitorcount").textContent = site_data.info.views;
+    document.getElementById("visitorcount").textContent = site_data.total_hits;
   }
 };
-xhttp.open("GET", "https://weirdscifi.ratiosemper.com/neocities.php?sitename=bytedrew", true);
+xhttp.open("GET", "https://hitscounter.dev/api/hit?output=json&url=https%3A%2F%2Fbytedrew.github.io%2F&label=Visitor&icon=award&color=%23198754&message=&style=flat&tz=UTC", true);
 xhttp.send();
